6afa61c56fc29ab27de0cd38f99f7631beb8bf19
edevregille <manudevregille@gmail.com>
2015-10-13 09:58:57 +0100

    v2

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ if(process.env.VCAP_SERVICES){
 	if (process.env.DB_PORT_27017_TCP_ADDR){
 		uri = "mongodb://" + process.env.DB_PORT_27017_TCP_ADDR + ":" + process.env.DB_PORT_27017_TCP_PORT +"/docker";
 	} else{
-		uri = "mongodb://edevregille:manu123@ds027613.mongolab.com:27613/IbmCloud_vtbiogtv_balfof6v"
+		uri = ""
 	}
 }
 
