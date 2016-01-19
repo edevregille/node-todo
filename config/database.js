e67e572fbac1272b6b4c88a399ce62f6e1fa9f4c
edevregille <manudevregille@gmail.com>
2016-01-19 14:25:24 +0000

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
 // MongoDB - connection URI
 if(process.env.VCAP_SERVICES){
 	var services = JSON.parse(process.env.VCAP_SERVICES);
-  if(services.mongolab) {
-    var uri = services.mongolab[0].credentials.uri;
-  } else {
-    uri = process.env.MONGO_URI;
-  }
+	if(services.mongolab) {
+		var uri = services.mongolab[0].credentials.uri;
+	} 
+	else if(services.mongodb-2.4){
+		var uri = services.mongodb-2.4[0].credentials.url;
+	}
+	else {
+		uri = process.env.MONGO_URI;
+	}
 } else {
 	if (process.env.DB_PORT_27017_TCP_ADDR){
 		uri = "mongodb://" + process.env.DB_PORT_27017_TCP_ADDR + ":" + process.env.DB_PORT_27017_TCP_PORT +"/docker";
